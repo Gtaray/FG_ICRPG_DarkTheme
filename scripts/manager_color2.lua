2cab6c58be7659aef16a7fae957bced578c99ffb
George Taray <Gtaray@Hotmail.com>
2021-04-16 09:37:28 -0700

    Updated to use new healthManager object in ICRPG ruleset.

--- scripts/manager_color2.lua
+++ scripts/manager_color2.lua
@@ -1,14 +1,14 @@
 
 function onInit()
-    ActorHealthManager.registerStatusHealthColor(ActorHealthManager.STATUS_UNCONSCIOUS, "1e1e1e");
-    ActorHealthManager.registerStatusHealthColor(ActorHealthManager.STATUS_DYING, "1e1e1e");
-    ActorHealthManager.registerStatusHealthColor(ActorHealthManager.STATUS_DEAD, "1e1e1e");
+    HealthManager.registerStatusHealthColor(HealthManager.STATUS_UNCONSCIOUS, "1e1e1e");
+    HealthManager.registerStatusHealthColor(HealthManager.STATUS_DYING, "1e1e1e");
+    HealthManager.registerStatusHealthColor(HealthManager.STATUS_DEAD, "1e1e1e");
 
-    ActorHealthManager.registerStatusHealthColor(ActorHealthManager.STATUS_HEALTHY, "57a64a");
-    ActorHealthManager.registerStatusHealthColor(ActorHealthManager.STATUS_LIGHT, "b8d7a3");
-    ActorHealthManager.registerStatusHealthColor(ActorHealthManager.STATUS_MODERATE, "dcdcaa");
-    ActorHealthManager.registerStatusHealthColor(ActorHealthManager.STATUS_HEAVY, "e67140");
-    ActorHealthManager.registerStatusHealthColor(ActorHealthManager.STATUS_CRITICAL, "f15151");
+    HealthManager.registerStatusHealthColor(HealthManager.STATUS_HEALTHY, "57a64a");
+    HealthManager.registerStatusHealthColor(HealthManager.STATUS_LIGHT, "b8d7a3");
+    HealthManager.registerStatusHealthColor(HealthManager.STATUS_MODERATE, "dcdcaa");
+    HealthManager.registerStatusHealthColor(HealthManager.STATUS_HEAVY, "e67140");
+    HealthManager.registerStatusHealthColor(HealthManager.STATUS_CRITICAL, "f15151");
 
-    ActorHealthManager.registerStatusHealthColor(ActorHealthManager.STATUS_SIMPLE_WOUNDED, "b8d7a3");
+    HealthManager.registerStatusHealthColor(HealthManager.STATUS_SIMPLE_WOUNDED, "b8d7a3");
 end
